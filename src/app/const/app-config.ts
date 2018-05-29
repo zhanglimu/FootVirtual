d75038caf0fda6a844e6de8063ff2e832a4ab1ff
zhangye <1756925172@qq.com>
2018-05-29 17:36:41 +0800

    config

--- src/app/const/app-config.ts
+++ src/app/const/app-config.ts
@@ -1,5 +1,3 @@
-export class AppConfig {
+﻿export class AppConfig {
     // static baseUrl: string = 'https://private-d8363-virtualfmsapi.apiary-mock.com';
-    // static baseUrl: string = 'http://fmsaws.jingcai.trade:12383';//测试
-    static baseUrl: string = 'http://192.168.1.6:8083';//1.5
 }
